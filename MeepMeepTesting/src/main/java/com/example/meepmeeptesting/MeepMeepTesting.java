ecff48a66fd193c22ec6f2ea3773e287d9b787c9
TAVii119 <tavigames@gmail.com>
2022-02-14 01:39:24 +0200

    Test code for trajectories.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,27 +1,30 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(480);
+        MeepMeep meepMeep = new MeepMeep(980);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(1051), Math.toRadians(360), 11.7)
+                .setConstraints(60, 60, Math.toRadians(1051), Math.toRadians(360), 10)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-12, -62, Math.toRadians(270)))
+                                .lineTo(new Vector2d(-12, -42))
+                                .lineToSplineHeading(new Pose2d(-61, -57, Math.toRadians(190)))
+                                .forward(2)
+                                .back(30)
+                                .splineToSplineHeading(new Pose2d(15, -68), Math.toRadians(0))
+                                .lineTo(new Vector2d(37, -68))
+                                .back(40)
+                                .lineToSplineHeading(new Pose2d(-12, -37, Math.toRadians(270)))
+                                .lineToSplineHeading(new Pose2d(5, -68, Math.toRadians(360)))
+                                .lineTo(new Vector2d(39, -68))
                                 .build()
                 );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousTest.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
-import android.graphics.Bitmap;
-
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -10,56 +8,39 @@ import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstra
 import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.function.Consumer;
-import org.firstinspires.ftc.robotcore.external.function.Continuation;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.FocusControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.PtzControl;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.Timing;
+import org.firstinspires.ftc.teamcode.Vision.BarCodeDetection;
+import org.firstinspires.ftc.teamcode.Vision.BarcodeUtil;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraBase;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
-import org.openftc.easyopencv.PipelineRecordingParameters;
-import org.outoftheboxrobotics.tensorflowapi.ImageClassification.TFICBuilder;
-import org.outoftheboxrobotics.tensorflowapi.ImageClassification.TensorImageClassifier;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
-@Disabled
-@Autonomous(name = "AutonomousTest")
+@Autonomous(name = "Autonomous Test")
 public class AutonomousTest extends LinearOpMode {
     private DcMotor intakeMotor;
     private DcMotor duckMotor;
 
     private Servo depositServo;
+    private Servo deposit2;
     private Servo iLifterServo;
     private Servo gbServoRight;
     private Servo gbServoLeft;
     private Servo tseServo;
 
+    BarcodeUtil webcamUtil;
+    BarCodeDetection.BarcodePosition TSEPosition;
+
     @Override
     public void runOpMode()
     {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcamUtil = new BarcodeUtil(hardwareMap, "Webcam 1", telemetry);
+        webcamUtil.init();
 
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -73,6 +54,7 @@ public class AutonomousTest extends LinearOpMode {
         duckMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         depositServo = hardwareMap.get(Servo.class, "depositServo");
+        deposit2 = hardwareMap.get(Servo.class, "deposit2Servo");
         iLifterServo = hardwareMap.get(Servo.class, "iLifterServo");
         gbServoLeft = hardwareMap.get(Servo.class, "gbServoLeft");
         gbServoRight = hardwareMap.get(Servo.class, "gbServoRight");
@@ -83,6 +65,7 @@ public class AutonomousTest extends LinearOpMode {
         tseServo.setDirection(Servo.Direction.REVERSE);
 
         depositServo.setPosition(0.16);
+        deposit2.setPosition(0);
         iLifterServo.setPosition(0);
         gbServoLeft.setPosition(0.03);
         gbServoRight.setPosition(0.03);
@@ -92,9 +75,11 @@ public class AutonomousTest extends LinearOpMode {
 
         while (opModeIsActive())
         {
+            TSEPosition = webcamUtil.getBarcodePosition();
             final int robotRadius = 9; // inches
             // Don't burn CPU cycles busy-looping in this sample
             sleep(50);
+
 //            if (pipeline.position == RingsDeterminationPipeline.RingPosition.NONE) {
 //                webcam.stopStreaming();
 //                webcam.stopRecordingPipeline();
@@ -117,40 +102,267 @@ public class AutonomousTest extends LinearOpMode {
 //                sleep(30000);
 //            }
 
-            caseC(drive);
+            if(TSEPosition == BarCodeDetection.BarcodePosition.RIGHT)
+                caseC(drive);
+            else if(TSEPosition == BarCodeDetection.BarcodePosition.MIDDLE)
+                caseB(drive);
+            else if(TSEPosition == BarCodeDetection.BarcodePosition.LEFT)
+                caseA(drive);
+            else
+                caseC(drive);
             sleep(30000);
         }
     }
 
-    Pose2d startPose = new Pose2d(-63.0, 49.0, Math.toRadians(0.0));
+    Pose2d startPose = new Pose2d(-12, -62, Math.toRadians(265.0));
+
+    // Y e spre perete
 
     private void caseC(SampleMecanumDrive drive) {
         drive.setPoseEstimate(startPose);
 
         // Declare trajectories
         Trajectory traj1 = drive.trajectoryBuilder(startPose)
-                .back(20)
+                .lineTo(new Vector2d(-12, -42))
                 .build();
 
-        Trajectory traj2 = drive.trajectoryBuilder(startPose)
-                .forward(5)
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(new Pose2d(-61, -57, Math.toRadians(190)))
+                .build();
+
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
+                .forward(2)
+                .build();
+
+        Trajectory traj4 = drive.trajectoryBuilder(traj3.end())
+                .back(30)
+                .lineToSplineHeading(new Pose2d(5, -55, Math.toRadians(355)))
+                .build();
+
+        Trajectory traj7 = drive.trajectoryBuilder(traj4.end())
+                .lineTo(new Vector2d(37, -68))
+                .build();
+
+        Trajectory traj8 = drive.trajectoryBuilder(traj7.end())
+                .back(40)
+                .lineToSplineHeading(new Pose2d(-12, -37, Math.toRadians(270)))
+                .build();
+
+
+        Trajectory traj11 = drive.trajectoryBuilder(traj8.end())
+                .lineToSplineHeading(new Pose2d(5, -68, Math.toRadians(360)))
+                .lineTo(new Vector2d(39, -68))
                 .build();
 
         // Run trajectories
         drive.followTrajectory(traj1);
         liftIntake();
-        sleep(250);
+        sleep(180);
         goToLevel3();
         sleep(1000);
         pushDeposit();
+        useIntake();
 
         drive.followTrajectory(traj2);
+        drive.followTrajectory(traj3);
 
+        runCarousel();
+        sleep(800);
+        runCarousel();
+
+        drive.followTrajectory(traj4);
+        useIntake();
+        drive.followTrajectory(traj7);
+        useIntake();
+        goToLevel3();
+        drive.followTrajectory(traj8);
+        pushDeposit();
+        drive.followTrajectory(traj11);
+        useIntake();
+    }
+
+    private void caseB(SampleMecanumDrive drive) {
+        drive.setPoseEstimate(startPose);
+
+        // Declare trajectories
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .lineTo(new Vector2d(-15, -51))
+                .build();
+
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(new Pose2d(-61, -57, Math.toRadians(190)))
+                .build();
+
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
+                .forward(1)
+                .build();
+
+        Trajectory traj4 = drive.trajectoryBuilder(traj3.end())
+                .back(30)
+                .build();
+
+        Trajectory traj5 = drive.trajectoryBuilder(traj4.end())
+                .lineToSplineHeading(new Pose2d(5, -55, Math.toRadians(355)))
+                .build();
+
+        Trajectory traj6 = drive.trajectoryBuilder(traj5.end())
+                .strafeRight(10)
+                .build();
+
+        Trajectory traj7 = drive.trajectoryBuilder(traj6.end())
+                .lineTo(new Vector2d(37, -68))
+                .build();
+
+        Trajectory traj8 = drive.trajectoryBuilder(traj7.end())
+                .back(40)
+                .build();
+
+        Trajectory traj9 = drive.trajectoryBuilder(traj8.end())
+                .strafeLeft(20)
+                .build();
+
+        Trajectory traj10 = drive.trajectoryBuilder(traj9.end())
+                .lineToSplineHeading(new Pose2d(-8, -33, Math.toRadians(250)))
+                .build();
+
+        Trajectory traj11 = drive.trajectoryBuilder(traj10.end())
+                .lineToSplineHeading(new Pose2d(5, -55, Math.toRadians(355)))
+                .build();
+
+        Trajectory traj12 = drive.trajectoryBuilder(traj11.end())
+                .lineToSplineHeading(new Pose2d(5, -68, Math.toRadians(330)))
+                .build();
+
+        Trajectory traj13 = drive.trajectoryBuilder(traj12.end())
+                .lineTo(new Vector2d(37, -68))
+                .build();
+
+        // Run trajectories
+        drive.followTrajectory(traj1);
+        liftIntake();
+        sleep(180);
+        goToLevel2();
+        sleep(1500);
+        pushDeposit();
+        useIntake();
+
+        drive.followTrajectory(traj2);
+        drive.followTrajectory(traj3);
+
+        runCarousel();
+        sleep(800);
+        runCarousel();
+
+        drive.followTrajectory(traj4);
+        drive.followTrajectory(traj5);
+        drive.followTrajectory(traj6);
+        useIntake();
+        drive.followTrajectory(traj7);
+        useIntake();
+        goToLevel3();
+        drive.followTrajectory(traj8);
+        drive.followTrajectory(traj9);
+        drive.followTrajectory(traj10);
+        pushDeposit();
+        drive.followTrajectory(traj11);
+        drive.followTrajectory(traj12);
+        useIntake();
+        drive.followTrajectory(traj13);
+    }
+
+    private void caseA(SampleMecanumDrive drive) {
+        drive.setPoseEstimate(startPose);
+
+        // Declare trajectories
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .lineTo(new Vector2d(-15, -51.5))
+                .build();
+
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                .lineToSplineHeading(new Pose2d(-61, -57, Math.toRadians(190)))
+                .build();
+
+        Trajectory traj3 = drive.trajectoryBuilder(traj2.end())
+                .forward(2)
+                .build();
+
+        Trajectory traj4 = drive.trajectoryBuilder(traj3.end())
+                .back(30)
+                .build();
+
+        Trajectory traj5 = drive.trajectoryBuilder(traj4.end())
+                .lineToSplineHeading(new Pose2d(5, -55, Math.toRadians(355)))
+                .build();
+
+        Trajectory traj6 = drive.trajectoryBuilder(traj5.end())
+                .strafeRight(10)
+                .build();
+
+        Trajectory traj7 = drive.trajectoryBuilder(traj6.end())
+                .lineTo(new Vector2d(37, -68))
+                .build();
+
+        Trajectory traj8 = drive.trajectoryBuilder(traj7.end())
+                .back(40)
+                .build();
+
+        Trajectory traj9 = drive.trajectoryBuilder(traj8.end())
+                .strafeLeft(20)
+                .build();
+
+        Trajectory traj10 = drive.trajectoryBuilder(traj9.end())
+                .lineToSplineHeading(new Pose2d(-6, -32, Math.toRadians(250)))
+                .build();
+
+        Trajectory traj11 = drive.trajectoryBuilder(traj10.end())
+                .lineToSplineHeading(new Pose2d(5, -55, Math.toRadians(355)))
+                .build();
+
+        Trajectory traj12 = drive.trajectoryBuilder(traj11.end())
+                .lineToSplineHeading(new Pose2d(5, -68, Math.toRadians(330)))
+                .build();
+
+        Trajectory traj13 = drive.trajectoryBuilder(traj12.end())
+                .lineTo(new Vector2d(37, -68))
+                .build();
+
+        // Run trajectories
+        drive.followTrajectory(traj1);
+        liftIntake();
+        sleep(180);
+        goToLevel1();
+        sleep(1800);
+        pushDeposit();
+        useIntake();
+
+        drive.followTrajectory(traj2);
+        drive.followTrajectory(traj3);
+
+        runCarousel();
+        sleep(800);
+        runCarousel();
+
+        drive.followTrajectory(traj4);
+        drive.followTrajectory(traj5);
+        drive.followTrajectory(traj6);
+        useIntake();
+        drive.followTrajectory(traj7);
+        useIntake();
+        goToLevel3();
+        drive.followTrajectory(traj8);
+        drive.followTrajectory(traj9);
+        drive.followTrajectory(traj10);
+        pushDeposit();
+        drive.followTrajectory(traj11);
+        drive.followTrajectory(traj12);
+        useIntake();
+        drive.followTrajectory(traj13);
     }
 
     public void goToLevel1() {
-        depositServo.setPosition(0.38);
-        intakeMotor.setPower(0.5);
+        deposit2.setPosition(0);
+        depositServo.setPosition(0.43);
+        intakeMotor.setPower(-0.5);
         iLifterServo.setPosition(0.38);
 
         gbServoRight.setPosition(0.83);
@@ -158,8 +370,9 @@ public class AutonomousTest extends LinearOpMode {
     }
 
     public void goToLevel2() {
-        depositServo.setPosition(0.38);
-        intakeMotor.setPower(0.5);
+        deposit2.setPosition(0);
+        depositServo.setPosition(0.43);
+        intakeMotor.setPower(-0.5);
         iLifterServo.setPosition(0.38);
 
         gbServoRight.setPosition(0.72);
@@ -167,28 +380,31 @@ public class AutonomousTest extends LinearOpMode {
     }
 
     public void goToLevel3() {
-        depositServo.setPosition(0.38);
-        intakeMotor.setPower(0.5);
+        deposit2.setPosition(0);
+        depositServo.setPosition(0.43);
+        intakeMotor.setPower(-0.5);
         iLifterServo.setPosition(0.38);
         sleep(500);
-        gbServoRight.setPosition(0.57);
-        gbServoLeft.setPosition(0.57);
+        gbServoRight.setPosition(0.58);
+        gbServoLeft.setPosition(0.58);
     }
 
     public void pushDeposit() {
+        deposit2.setPosition(0.28);
         depositServo.setPosition(0.64);
 
         // Wait for minerals to be ejected from deposit
-        sleep(250);
+        sleep(300);
 
         gbServoLeft.setPosition(0.03);
         gbServoRight.setPosition(0.03);
         intakeMotor.setPower(0.5);
         depositServo.setPosition(0.16);
 
-        sleep(650);
+        sleep(1100);
 
         iLifterServo.setPosition(0.20);
+        deposit2.setPosition(0);
     }
 
     public void useIntake() {
