1c814d43bf70118612b0248d38865efc407ef8ee
TAVii119 <tavigames@gmail.com>
2022-03-01 10:52:47 +0200

    Removed roadrunner and restarting tuning.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OdometryTester.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+//@Disabled
+@TeleOp(name="Odometry Tester", group="Test")
+public class OdometryTester extends LinearOpMode {
+
+    DcMotor leftEncoder, rightEncoder, strafeEncoder;
+
+    @Override
+    public void runOpMode() {
+
+        leftEncoder = hardwareMap.get(DcMotor.class, "lf");
+        rightEncoder = hardwareMap.get(DcMotor.class, "rf");
+        strafeEncoder = hardwareMap.get(DcMotor.class, "lb");
+
+        leftEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        strafeEncoder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        waitForStart();
+        while(opModeIsActive()) {
+            telemetry.addData("leftEncoder", leftEncoder.getCurrentPosition());
+            telemetry.addData("rightEncoder", rightEncoder.getCurrentPosition());
+            telemetry.addData("strafeEncoder", strafeEncoder.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/BarCodeDetection.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class BarCodeDetection extends OpenCvPipeline {
+
+    Telemetry telemetry;
+    Mat mat = new Mat( );
+
+    public enum BarcodePosition {
+        LEFT,
+        MIDDLE,
+        RIGHT,
+        NOT_FOUND
+    }
+
+    private BarcodePosition barcodePosition;
+
+    static final Rect LEFT_ROW = new Rect(
+            new Point( 476, 357 ),
+            new Point( 580, 471 ) );
+    static final Rect MIDDLE_ROW = new Rect(
+            new Point( 638, 433 ),
+            new Point( 763, 575 ) );
+    static final Rect RIGHT_ROW = new Rect(
+            new Point( 878, 566 ),
+            new Point( 1029, 713 ) );
+
+    static double PERCENT_COLOR_THRESHOLD = 0.1;
+
+    public BarCodeDetection(Telemetry t ) {
+        telemetry = t;
+    }
+
+    public Mat processFrame( Mat input, String type ) {
+
+        Imgproc.cvtColor( input, mat, Imgproc.COLOR_RGB2HSV );
+        Scalar lowHSV;
+        Scalar highHSV;
+
+        /*if( type.equalsIgnoreCase( "TSE" ) ) {
+            lowHSV = new Scalar( 25, 25, 35 );
+            highHSV = new Scalar( 40, 255, 255 );
+        } else {
+            lowHSV = new Scalar( 40, 50, 70 );
+            highHSV = new Scalar( 65, 255, 255 );
+        }*/
+        lowHSV = new Scalar(10, 100, 20);
+        highHSV = new Scalar(25, 255, 255);
+        Core.inRange( mat, lowHSV, highHSV, mat );
+
+        Mat left = mat.submat(LEFT_ROW);
+        Mat middle = mat.submat(MIDDLE_ROW);
+        Mat right = mat.submat(RIGHT_ROW);
+
+        double leftValue = Core.sumElems( left ).val[0] / LEFT_ROW.area( ) / 255;
+        double middleValue = Core.sumElems( middle ).val[0] / MIDDLE_ROW.area( ) / 255;
+        double rightValue = Core.sumElems( right ).val[0] / RIGHT_ROW.area( ) / 255;
+
+        left.release( );
+        middle.release( );
+        right.release( );
+
+        boolean leftBool = leftValue > PERCENT_COLOR_THRESHOLD;
+        boolean middleBool = middleValue > PERCENT_COLOR_THRESHOLD;
+        boolean rightBool = rightValue > PERCENT_COLOR_THRESHOLD;
+
+        if( rightBool ) {
+            barcodePosition = BarcodePosition.RIGHT;
+            telemetry.addData( "Location", type + " right" );
+        } else if( leftBool ) {
+            barcodePosition = BarcodePosition.LEFT;
+            telemetry.addData( "Location", type + " left" );
+        } else if( middleBool ) {
+            barcodePosition = BarcodePosition.MIDDLE;
+            telemetry.addData( "Location", type + " middle" );
+        } else {
+            barcodePosition = BarcodePosition.NOT_FOUND;
+            telemetry.addData( "Location", type + " not found" );
+        }
+        Imgproc.cvtColor( mat, mat, Imgproc.COLOR_GRAY2RGB );
+
+        Scalar elementColor = new Scalar( 255, 0, 0 );
+        Scalar notElement = new Scalar( 0, 255, 0 );
+
+        Imgproc.rectangle( mat, LEFT_ROW, barcodePosition == BarcodePosition.LEFT ? notElement : elementColor );
+        Imgproc.rectangle( mat, RIGHT_ROW, barcodePosition == BarcodePosition.RIGHT ? notElement : elementColor );
+        Imgproc.rectangle( mat, MIDDLE_ROW, barcodePosition == BarcodePosition.MIDDLE ? notElement : elementColor );
+        return mat;
+    }
+
+    @Override
+    public Mat processFrame( Mat input ) {
+
+        Mat elementImage = processFrame( input, "element" );
+        Mat duckImage = processFrame( input, "duck" );
+        double elementValue = Core.sumElems( elementImage ).val[0] / (elementImage.rows( ) * elementImage.cols( )) / 255;
+        double duckValue = Core.sumElems( duckImage ).val[0] / (duckImage.rows( ) * duckImage.cols( )) / 255;
+        telemetry.update( );
+        if( elementValue < duckValue )
+            return duckImage;
+        return elementImage;
+    }
+
+    public BarcodePosition getBarcodePosition( ) {
+        return barcodePosition;
+    }
+}
